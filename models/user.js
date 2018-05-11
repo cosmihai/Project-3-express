ea03492027b93bc6b968988afb6827db44f4fc77
cosmihai <cos_mihai23@yahoo.com>
2018-05-11 16:10:06 +0200

    new user schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,22 @@
 'use strict'
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  pictureUrl: {
+    type:String,
+    default: 'https://s3.amazonaws.com/thumbnails.illustrationsource.com/huge.49.247675.JPG'
+  },
+  firstName: String,
+  lastName: String,
+  myCocktails: [{
+    type: ObjectId,
+    ref: 'cocktail'
+  }]
 });
 
 const User = mongoose.model('User', userSchema);
